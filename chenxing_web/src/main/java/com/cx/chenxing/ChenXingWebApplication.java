76a0634f45962c63c3a5281d9d2d1ea61a4e24e2
pengdeman <872422345@qq.com>
2018-09-20 16:39:59 +0800

    优化

--- chenxing_web/src/main/java/com/cx/chenxing/ChenXingWebApplication.java
+++ chenxing_web/src/main/java/com/cx/chenxing/ChenXingWebApplication.java
@@ -4,9 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@MapperScan("com.cx.chenxing.user.dao")
+@MapperScan("com.cx.chenxing.*.dao")
 @ComponentScan(basePackages= {"com.cx.chenxing"})
 public class ChenXingWebApplication {
 	public static void main(String[] args) {
