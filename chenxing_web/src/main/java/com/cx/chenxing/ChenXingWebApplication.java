e3f0b4bd6541d22c3974eef432ff025226b0e225
pengdeman <pdm@920521>
2020-02-14 21:47:00 +0800

    修改为jar包启动 java -jar

--- chenxing_web/src/main/java/com/cx/chenxing/ChenXingWebApplication.java
+++ chenxing_web/src/main/java/com/cx/chenxing/ChenXingWebApplication.java
@@ -3,13 +3,15 @@ package com.cx.chenxing;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.cx.chenxing.*.dao")
-@ComponentScan(basePackages= {"com.cx.chenxing"})
+//@ComponentScan(basePackages= {"com.cx.chenxing"})
 public class ChenXingWebApplication extends SpringBootServletInitializer {
 
 	@Override
